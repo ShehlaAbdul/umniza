a3a1ed9e04cf7d22881865181bd975e8ccb050bc
Shahla <shahlaabdullayevaa@gmail.com>
2026-02-16 18:01:33 +0400

    tailwind css added

--- src/Layout/Footer/Footer.jsx
+++ src/Layout/Footer/Footer.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import "./Style.scss";
+import { PiInstagramLogoLight } from "react-icons/pi";
+
 
 function Footer() {
   return (
     <div>
-      footer
+      <div className="footer">
+
+      <PiInstagramLogoLight />
+      </div>
     </div>
   );
 }
